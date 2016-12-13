6df7993397a00d30585c4b6909cb6fcf523ffa63
Dave <uDave@Main.local>
2016-12-13 14:28:35 +0200

    Second commit

--- server.js
+++ server.js
@@ -10,6 +10,9 @@ app.use(bodyParser.json());
 
 app.use(passport.initialize());
 
+app.use(express.static('/'));
+app.use(express.static('node_modules'));
+
 passport.use('login', new LocalStrategy(function (username, password, done) {
   var authenticated = username === "John" && password === "Smith";
   
@@ -19,3 +22,25 @@ passport.use('login', new LocalStrategy(function (username, password, done) {
     return done(null, false);       
   }
 }));
+
+// Let’s add a '/login' POST route with passport.authenticate as 
+// the route-level middleware. And inside of the route-level 
+// middleware, we'll pass the name of the strategy ('login') and 
+// callback functions to change routes based on the success or 
+// failure of the authentication:
+app.post('/login', passport.authenticate('login', {
+  successRedirect: '/success',
+  failureRedirect: '/login',
+  session: false
+}));
+
+app.get('/success', function (req, res){
+  res.send("Hey, hello from the server!");
+});
+
+app.get('/login', function (req, res) {
+  res.sendFile(__dirname + '/login.html');
+});
+
+
+app.listen(8000);
